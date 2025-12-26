9a8c12de75bc7e6f51f715b13ed06ff44d251281
YaroslavPavliga <st9260851@stud.duikt.edu.ua>
2025-12-26 22:51:10 +0200

    Create jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+'EOF'
+module.exports = {
+  testEnvironment: 'node',
+  testMatch: ['**/tests/**/*.js', '**/?(*.)+(spec|test).js'],
+  verbose: true,
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+};
+EOF
