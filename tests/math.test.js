c0c04351ae598725bd3723e60d634323dd3d9d2b
YaroslavPavliga <st9260851@stud.duikt.edu.ua>
2025-12-26 22:53:56 +0200

    Create math.test.js

---
+++ tests/math.test.js
@@ -0,0 +1,14 @@
+'EOF'
+// Прості математичні тести
+test('1 + 1 equals 2', () => {
+  expect(1 + 1).toBe(2);
+});
+
+test('2 * 3 equals 6', () => {
+  expect(2 * 3).toBe(6);
+});
+
+test('10 / 2 equals 5', () => {
+  expect(10 / 2).toBe(5);
+});
+EOF
