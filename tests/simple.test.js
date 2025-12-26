5f7392d93920a1332f76b58118b8e5cdfa6c1d53
YaroslavPavliga <st9260851@stud.duikt.edu.ua>
2025-12-26 22:41:00 +0200

    Create simple.test.js

---
+++ tests/simple.test.js
@@ -0,0 +1,10 @@
+// Найпростіший тест для перевірки, що Jest працює
+describe('Basic Math Tests', () => {
+  test('1 + 1 should equal 2', () => {
+    expect(1 + 1).toBe(2);
+  });
+
+  test('2 * 3 should equal 6', () => {
+    expect(2 * 3).toBe(6);
+  });
+});
